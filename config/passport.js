0afe19381e5159a267ed8a1d179f09eb495cd437
DriesVerb <verboven.dries@gmail.com>
2021-06-15 10:53:06 +0200

    my version

--- config/passport.js
+++ config/passport.js
@@ -1,4 +1,4 @@
-const User = require("..models/User");
+const User = require("../models/User");
 
 //Local Strategy
 const LocalStrategy = require("passport-local").Strategy;
--- routes/logInRouter.js
+++ routes/logInRouter.js
@@ -2,7 +2,7 @@ const express = require(`express`);
 const router = express.Router();
 const logInController = require("../controllers/signUpController");
 
-router.get('/', logInController.logIn);
-router.post('/', logInController.logInPost);
+router.get("/", logInController.logIn);
+router.post("/", logInController.logInPost);
 
 module.exports = router;
