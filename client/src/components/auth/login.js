9a18b3c94c64c056a4fbc35c6023b1d4f444ad76
DriesVerb <verboven.dries@gmail.com>
2021-06-18 10:15:44 +0200

    added last version

--- client/src/components/auth/login.js
+++ client/src/components/auth/login.js
@@ -1,54 +1,64 @@
-import React, { useState, useContext, useEffect } from 'react';
-import axios from 'axios'
+import React, { useState, useContext, useEffect } from "react";
+import axios from "axios";
 // import AuthContext from '../../context/auth/authContext';
 
 function Login() {
-    
-    // const authContext = useContext(AuthContext);
+  // const authContext = useContext(AuthContext);
 
-    const [user, setUser] = useState({
-        email: "",
-        password: ""
-    })
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
 
-    const getData = (e) =>{
-        setUser({
-            ...user,
-            [e.target.name] : e.target.value
-        })
-    }
+  const getData = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    const submToServer = (e)=>{
-        e.preventDefault();
-        axios.post('/' /* Here the Route */, user )
-        .then(res=>{
-            console.log(res.data)
-        })
-        .catch(err=>{
-            console.log(err)
-        })
-    }
-    
+  const submToServer = (e) => {
+    e.preventDefault();
+    axios
+      .post("/" /* Here the Route */, user)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-
-
-
-    return (
-        <div>
-            <form onSubmit={submToServer}>
-                <h2>Login</h2>
-                <label htmlFor="email">Email</label>
-                <input type="email" name="email" placeholder="Your Email.." onChange={getData}/>
-                <br />
-                <label htmlFor="password">Password</label>
-                <input type="password" name="password" placeholder="Your Password.." onChange={getData}/>
-                <button type="submit" className="btn btn-success">Login</button>
-            </form>
-                <button>Facebook</button>
-                <button>Instagram</button>
-                <button>Github</button>
-        </div>
-    )
+  return (
+    <div>
+      <form onSubmit={submToServer}>
+        <h2>Login</h2>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          name="email"
+          placeholder="Your Email.."
+          onChange={getData}
+          defaultValue="hello@hi.com"
+        />
+        <br />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          placeholder="Your Password.."
+          onChange={getData}
+          defaultValue="123456"
+        />
+        <button type="submit" className="btn btn-success">
+          Login
+        </button>
+      </form>
+      <button>Facebook</button>
+      <button>Instagram</button>
+      <button>Github</button>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,24 +1,21 @@
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 module.exports = function (req, res, next) {
-  //if there is a token in the header put it in the request 
+  //if there is a token in the header put it in the request
 
-  const token = req.header('x-auth-token');
-
+  const token = req.header("x-token");
 
   // if there is no token
 
   if (!token) {
-    return res.status(401).json({ msg: 'No Token , authorization denied' });
+    return res.status(401).json({ msg: "No Token , authorization denied" });
   }
 
   //validate token
   try {
-
     const jwtSecret = process.env.JWT_SECRET;
     const decoded = jwt.verify(token, jwtSecret); //payload object will be in the decoded
-
 
     //sign the user in the request
 
@@ -26,6 +23,6 @@ module.exports = function (req, res, next) {
 
     next();
   } catch (err) {
-    res.status(401).json({ msg: 'Token in not valid ' });
+    res.status(401).json({ msg: "Token in not valid " });
   }
 };
