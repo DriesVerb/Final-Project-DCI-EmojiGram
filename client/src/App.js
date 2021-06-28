f1b4a2549f6cf6e1f7c321f9f4a0faabd8eb876e
DriesVerb <verboven.dries@gmail.com>
2021-06-28 10:34:52 +0200

    solving bug of merge

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,9 @@
-import React, { useEffect, useContext } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React, { useEffect, useContext } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 // components
 
-import Alerts from './components/Alert';
-import Navbar from './components/Navbar';
-import Login from './components/auth/login';
-import SignUp from './components/auth/SignUp';
-import LandingPage from './components/layout/LandingPage';
-import PrivateRoute from './components/privateRoute';
-import HomePublic from './components/layout/HomePublic';
-import Footer from './components/Footer';
-import StoryEditor from './components/story/StoryEditor';
-import UserInterface from './components/userInterface';
-import ContactUs from './components/ContactUs'
-
-// context
-
-import AuthContext from './context/auth/authContext';
-import AlertState from './context/alert/alertState';
-import './App.css';
-import Alerts from "./components/Alert";
 import Navbar from "./components/Navbar";
 import Login from "./components/auth/login";
 import SignUp from "./components/auth/SignUp";
@@ -29,19 +11,23 @@ import LandingPage from "./components/layout/LandingPage";
 import PrivateRoute from "./components/privateRoute";
 import HomePublic from "./components/layout/HomePublic";
 import Footer from "./components/Footer";
-import YourStories from "./components/story/yourStories";
 import StoryEditor from "./components/story/StoryEditor";
-import PreviewStory from "./components/story/PreviewStory";
 import UserInterface from "./components/userInterface";
+import ContactUs from "./components/ContactUs";
+import YourStories from "./components/story/yourStories";
+import PreviewStory from "./components/story/PreviewStory";
 import ShowStory from "./components/story/showStory";
+
 // context
-import StoryState from "./context/story/storyState";
+
 import AuthContext from "./context/auth/authContext";
 import AlertState from "./context/alert/alertState";
+import StoryState from "./context/story/storyState";
 import "./App.css";
+import Alerts from "./components/Alert";
 
 // import setAuthToken
-import setAuthToken from './context/auth/setAuthToken';
+import setAuthToken from "./context/auth/setAuthToken";
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -56,28 +42,32 @@ function App() {
 
   return (
     <StoryState>
-    <AlertState>
-      <div className='App'>
-        <Router>
-          <Navbar />
-          <Alerts />
-          <Switch>
-            <Route path="/" exact component={HomePublic} />
-            <PrivateRoute path="/landing" exact component={LandingPage} />
-            <PrivateRoute path="/yourstories" exact component={YourStories} />
-            <PrivateRoute path="/writestory" exact component={StoryEditor} />
-            <PrivateRoute path="/previewstory" exact component={PreviewStory} />
-            <Route path="/showstory/:id" exact component={ShowStory} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={SignUp} />
-            <PrivateRoute path="/UserInterface" component={UserInterface} />
-          </Switch>
-          <Footer />
-          <Route path='/contactus' component={ContactUs} />
-        </Router>
-      </div>
+      <AlertState>
+        <div className="App">
+          <Router>
+            <Navbar />
+            <Alerts />
+            <Switch>
+              <Route path="/" exact component={HomePublic} />
+              <PrivateRoute path="/landing" exact component={LandingPage} />
+              <PrivateRoute path="/yourstories" exact component={YourStories} />
+              <PrivateRoute path="/writestory" exact component={StoryEditor} />
+              <PrivateRoute
+                path="/previewstory"
+                exact
+                component={PreviewStory}
+              />
+              <Route path="/showstory/:id" exact component={ShowStory} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={SignUp} />
+              <PrivateRoute path="/UserInterface" component={UserInterface} />
+            </Switch>
+            <Footer />
+            <Route path="/contactus" component={ContactUs} />
+          </Router>
+        </div>
       </AlertState>
-      </StoryState>
+    </StoryState>
   );
 }
 
