735653a6e03b88be8d7538309ef021f9853aacbf
DriesVerb <verboven.dries@gmail.com>
2021-07-09 09:08:35 +0200

    added some new styling

--- client/src/components/auth/login.js
+++ client/src/components/auth/login.js
@@ -2,7 +2,6 @@ import React, { useState, useContext, useEffect } from "react";
 import AlertContext from "../../context/alert/alertContext";
 import AuthContext from "../../context/auth/authContext";
 
-
 function Login(props) {
   const authContext = useContext(AuthContext);
   const alertContext = useContext(AlertContext);
@@ -52,57 +51,58 @@ function Login(props) {
     }
   };
   return (
-    <div>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          submToServer();
-        }}
-      >
-        <h2>Login</h2>
-        <label htmlFor="email">Email</label>
-        <input
-          value={email}
-          type="text"
-          name="email"
-          placeholder="Your Email.."
-          onChange={getData}
-        />
-        <br />
-        <label htmlFor="password">Password</label>
-        <input
-          value={password}
-          type="password"
-          name="password"
-          placeholder="Your Password.."
-          onChange={getData}
-        />
-        <button type="submit" className="btn btn-success">
-          Login
-        </button>
-
-      </form>
-      {/* <button>Facebook</button>
-      <button>Instagram</button>
-      <button>Github</button> */}
-      <a
-        href="http://localhost:5000/login/passport/github"
-        className="fab-fa-github btn btn-dark font-weight-bold "
-      >
-        Github
-      </a>
-      <a
-        href="http://localhost:5000/login/passport/instagram"
-        className="btn btn-danger ml-4 font-weight-bold"
-      >
-        Instagram
-      </a>
-      <a
-        href="http://localhost:5000/login/passport/facebook"
-        className="btn btn-primary ml-4 font-weight-bold "
-      >
-        Facebook
-      </a>
+    <div className="grid-container">
+      <div className="grid-container__mid">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            submToServer();
+          }}
+        >
+          <h2>Login</h2>
+          <label htmlFor="email">Email</label>
+          <input
+            value={email}
+            type="text"
+            name="email"
+            placeholder="Your Email.."
+            onChange={getData}
+          />
+          <br />
+          <label htmlFor="password">Password</label>
+          <input
+            value={password}
+            type="password"
+            name="password"
+            placeholder="Your Password.."
+            onChange={getData}
+          />
+          <button type="submit" className="btn btn-success">
+            Login
+          </button>
+        </form>
+        {/* <button>Facebook</button>
+    <button>Instagram</button>
+    <button>Github</button> */}
+        <a
+          href="http://localhost:5000/login/passport/github"
+          className="fab-fa-github btn btn-dark font-weight-bold "
+        >
+          Github
+        </a>
+        <a
+          href="http://localhost:5000/login/passport/instagram"
+          className="btn btn-danger ml-4 font-weight-bold"
+        >
+          Instagram
+        </a>
+        <a
+          href="http://localhost:5000/login/passport/facebook"
+          className="btn btn-primary ml-4 font-weight-bold "
+        >
+          Facebook
+        </a>
+      </div>
     </div>
   );
 }
