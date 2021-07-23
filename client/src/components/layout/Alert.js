c0c39354c0ef242f9d1a03e3538413c620a5632b
DriesVerb <verboven.dries@gmail.com>
2021-07-23 15:25:56 +0200

    add changes brujo

--- client/src/components/layout/Alert.js
+++ client/src/components/layout/Alert.js
@@ -1,6 +1,5 @@
-import React, { useContext } from "react";
-import AlertContext from "../../context/alert/alertContext";
-import { Alert, Col, Row } from "react-bootstrap";
+import React, { useContext } from 'react';
+import AlertContext from '../../context/alert/alertContext';
 
 const Alerts = () => {
   const alertContext = useContext(AlertContext);
@@ -11,18 +10,16 @@ const Alerts = () => {
     //if the alert state has something then loop in it
     alerts.length > 0 &&
     alerts.map((alert) => (
-      <Alert
-        key={alert.id}
-        variant={`${alert.alertStyle}`}
-        className="app__navbar"
-      >
-        <Row>
-          <Col md={{ span: 8, offset: 3 }} className="fas fa-info-circle">
-            <span>{""} </span>
-            {alert.msg}{" "}
-          </Col>
-        </Row>
-      </Alert>
+      <div className="app__alert">
+        <div className="alert-side">
+          <div
+            className={`alert-popup alert-popup__${alert.alertStyle}`}
+            key={alert.id}
+          >
+            <div className="alert-popup__msg">{alert.msg}</div>
+          </div>
+        </div>
+      </div>
     ))
   );
 };
--- client/src/components/pages/userInterface.js
+++ client/src/components/pages/userInterface.js
@@ -1,38 +1,51 @@
-import React, { useContext} from "react";
-import Images from "../../img/minions1.jpg";
-import { Link,  } from "react-router-dom";
-import AuthContext from "../../context/auth/authContext";
+import React, { useContext } from 'react';
+import Images from '../../img/minions1.jpg';
+import { Link } from 'react-router-dom';
+import AuthContext from '../../context/auth/authContext';
+
+// components
+import Icon from '../utils/Icon';
+
 // Navbar user interface
 function userInterface(props) {
-
   const authContext = useContext(AuthContext);
 
-  const { user} = authContext;
+  const { user } = authContext;
 
   // let history = useHistory();
 
   const toMyStories = (id) => {
-    
     props.history.push(`/yourstories/${id}`);
-    console.log(id)
+    console.log(id);
   };
 
   return (
     <div className="grid-container">
       <div className="grid-container__mid">
         <div className="user-interface__cards">
-          <div className="user-card">
-            <img className="user-card__img" src={Images} alt="userImage"></img>
+          <div
+            className="user-card"
+            onClick={(e) => {
+              props.history.push(`/writestory`);
+            }}
+          >
+            <Icon name="quill" color="blue" size="large" />
             <p className="user-card__title">CREATE YOUR STORY</p>
+
             <Link to="/writestory" className="link">
               <button className="btn btn-success user-card__btn">
                 Click to Create
               </button>
             </Link>
           </div>
-          <div className="user-card">
-            <img className="user-card__img" src={Images} alt="userImage"></img>
 
+          <div
+            className="user-card"
+            onClick={(e) => {
+              props.history.push(`/friendsWall`);
+            }}
+          >
+            <Icon name="users" color="blue" size="large" />
             <p className="user-card__title">READ FRIENDS STORIES</p>
             <Link to="/friendsWall" className="link">
               <button className="btn btn-success user-card__btn">
@@ -40,11 +53,10 @@ function userInterface(props) {
               </button>
             </Link>
           </div>
-          <div className="user-card">
-            <img className="user-card__img" src={Images} alt="userImage"></img>
-
+          <div className="user-card" onClick={() => toMyStories(user._id)}>
+            <Icon name="collections_bookmark" color="blue" size="large" />
             <p className="user-card__title">ACCESS YOUR STORIES</p>
-            <div onClick={() => toMyStories(user._id)}  className="link">
+            <div onClick={() => toMyStories(user._id)} className="link">
               <button className="btn btn-success user-card__btn">
                 Click to Access
               </button>
