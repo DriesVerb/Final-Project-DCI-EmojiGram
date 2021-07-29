8dc78a95341cd70b106ecb3f707aeec27bdccd82
DriesVerb <verboven.dries@gmail.com>
2021-07-29 14:17:03 +0200

    added fixes to styling

--- client/src/components/layout/SideNavBar.js
+++ client/src/components/layout/SideNavBar.js
@@ -5,18 +5,20 @@ const SideNavBar = () => {
   return (
     <div className="app-grid__navbar--side">
       <div className="side-navbar">
-        <Link to="/" className="side-navbar__link" href="/">
-          Home
-        </Link>
-        <Link to="/genre" className="side-navbar__link" href="/genre">
-          Genres
-        </Link>
-        <Link className="side-navbar__link" href="/genre">
-          About
-        </Link>
-        <Link className="side-navbar__link" href="/genre">
-          Contact
-        </Link>
+        <div className="side-navbar__links">
+          <Link to="/" className="side-navbar__link" href="/">
+            Home
+          </Link>
+          <Link to="/genre" className="side-navbar__link" href="/genre">
+            Genres
+          </Link>
+          <Link className="side-navbar__link" href="/genre">
+            About
+          </Link>
+          <Link className="side-navbar__link" href="/genre">
+            Contact
+          </Link>
+        </div>
       </div>
     </div>
   );
