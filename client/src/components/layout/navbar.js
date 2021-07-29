14bde92b855626ee6380d561635c41e69e610276
Lara_Ziyad <laraziyad.art@gmail.com>
2021-07-29 12:05:35 +0200

    pull

--- client/src/components/layout/navbar.js
+++ client/src/components/layout/navbar.js
@@ -24,13 +24,13 @@ function navbar() {
             <a className="nav-link text-dark" href="#!">
              <p> Hello{" "}</p>
               {user && <div>
-                <img
+                {/* <img
                   width="50px" height="60px"
                             //  style =" width:30px  height:10px "
                             className="round-img"
                             src={user.avatar}
                             alt=""
-                          />
+                          /> */}
               <p>{ user.username.charAt(0).toUpperCase() + user.username.slice(1)}</p> </div>}
               <span className="sr-only">(current)</span>
             </a>
