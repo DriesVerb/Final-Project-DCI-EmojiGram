53d8f9be27d75c6f751991ab81a340427cca8706
Lara_Ziyad <laraziyad.art@gmail.com>
2021-07-29 14:09:25 +0200

    pull

--- controllers/storyController.js
+++ controllers/storyController.js
@@ -284,18 +284,20 @@ exports.removeComment = async (req, res) => {
     const comment = story.comments.find(
       (comment) => comment.id === req.params.comment_id
     );
+    console.log (story)
     // Make sure comment exists
     if (!comment) {
       return res.status(404).json({ msg: 'Comment does not exist' });
     }
     // Check user
-    if (comment.user.toString() !== req.user.id) {
+    if (comment.user.toString() !== req.user.id  ) {
       return res.status(401).json({ msg: 'User not authorized' });
     }
-
+   
+else if (comment.user.toString() == req.story.user  ){
     story.comments = story.comments.filter(
       ({ id }) => id !== req.params.comment_id
-    );
+    )};
 
     await story.save();
 
